5a18ba67ce4acbbf9fdfa02d1cabad7e600d6275
Allison Ricker <alliricker@gmail.com>
2020-10-05 17:43:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
 class Dog
 
-  def initialize(name)
-  @name = name
+  def initialize(breed)
+    @breed = breed
   end
 
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed 
+    @breed
+  end
+  
 end
